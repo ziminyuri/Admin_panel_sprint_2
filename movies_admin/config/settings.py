6353341ef0dc3acd3c2eff6311ef562ffacf0509
ziminyuri <zimin.yuri22@gmail.com>
2022-01-02 20:58:41 +0700

    :construction: Докеризация PG

--- movies_admin/config/settings.py
+++ movies_admin/config/settings.py
@@ -56,22 +56,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-
-    #  'default': {
-    #     'ENGINE': env('DB_ENGINE'),
-    #     'NAME': env('DB_NAME'),
-    #     'USER': env('DB_USER'),
-    #     'PASSWORD': env('DB_PASSWORD'),
-    #     'HOST': env('DB_HOST'),
-    #     'PORT': env('DB_PORT'),
-    #     'OPTIONS': {
-    #        'options': '-c search_path=public,content'
-    #     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+     'default': {
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+        'OPTIONS': {
+           'options': '-c search_path=public,content'
+        }
+    }
 }
 
 
